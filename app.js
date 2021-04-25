7141eb0c8f8d1f827a1d80fb66af510beb06d174
coltyngregory <68095960+coltyngregory@users.noreply.github.com>
2021-04-24 20:29:18 -0700

    app.js
    '/' and '/presets' endpoints are working.
    
    '/presets' is responding with JSON to our react project's fetch request

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const cors = require('cors')
+const PORT = 5000
+
+app.use(cors())
+
+const preset = "This is a test preset"
 
 app.get('/', (req, res) => {
-	res.send('made it')
+	res.send('Server is running')
+})
+
+app.get('/presets', (req, res) => {
+	res.json({ myPreset: preset });
 })
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
